9e81d23d124d6868a85ab3c8817c9b1d1f3c8811
diazmanne <10931032+diazmanne@users.noreply.github.com>
2025-02-03 14:14:31 -0600

    satge_01 updated

--- src/cnnClassifier/pipeline/stage_01_data_ingestion.py
+++ src/cnnClassifier/pipeline/stage_01_data_ingestion.py
@@ -2,17 +2,27 @@ from cnnClassifier.config.configuration import ConfigurationManager
 from cnnClassifier.components.data_ingestion import DataIngestion
 from cnnClassifier import logger
 
-class DataIngestionPipeline:
-    def __init__(self):
-        self.config = ConfigurationManager()
 
+STAGE_NAME = "Data Ingestion stage"
+
+class DataIngestionTrainingPipeline:
+    def __init__(self):
+        pass
+    
     def main(self):
-        try:
-            data_ingestion_config = self.config.get_data_ingestion_config()
-            data_ingestion = DataIngestion(config=data_ingestion_config)
-            data_ingestion.download_file()
-            data_ingestion.extract_zip_file()
-            logger.info("Data ingestion pipeline completed successfully.")
-        except Exception as e:
-            logger.exception("An error occurred during the data ingestion pipeline.")
-            raise e
+        config = ConfigurationManager()
+        data_ingestion_config = config.get_data_ingestion_config()
+        data_ingestion = DataIngestion(config=data_ingestion_config)
+        data_ingestion.download_file()
+        data_ingestion.extract_zip_file()
+
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
